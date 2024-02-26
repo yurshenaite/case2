12c38eb937d12c8079793e040f678ab75dfa7162
Анастасия <a.yurshenaite@g.nsu.ru>
2024-02-26 10:25:08 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Part of case-study #2: Air balloon
+# Developers: Khramchikhina A., Peshkov Y., Sanzhanova A., Yurshenaite A.
+
+import main as ru
+
+
+def main():
+    '''
+    Main function.
+    :return: None
+    '''
+
+    mass_skin = float(input(ru.MASS_SKIN))
+    volume_l = float(input(ru.VOLUME_L))
+    t_outside_c = float(input(ru.T_OUTSIDE_C))
+    pressure = 10**5
+    molar_mass = 29 * (10**(-3))
+    gas_constant = 8.31
+    specific_heat = 1_000
+
+
+
+
